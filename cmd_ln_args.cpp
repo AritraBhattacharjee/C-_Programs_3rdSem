b2c7567000696421933c5b60387ee1a67224a943
ARITRA BHATTACHARJEE <aritrabhattacharjee321@gmail.com>
2021-11-23 15:20:21 +0530

    Programs for 23/11/21

---
+++ cmd_ln_args.cpp
@@ -0,0 +1,59 @@
+/*5.	Write a C++ program to handle command line arguments using argc and argv[]. Take a set of numbers as input through an int array. Then create two files ORIGINAL and SORTED using the command line arguments and write the original array to the file ORIGINAL and the sorted array to the file SORTED. The program finally displays the contents of both the files*/
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ofstream f1, i1, i2;
+    ifstream ifile;
+    int k = 0;
+    int *arr = new int[argc - 1];
+    i1.open("ORIGINAL");
+    for (int i = 1; i <= argc; i++)
+    {
+        arr[k] = atoi(argv[i]);
+        i1 << arr[k]<<endl;
+        k++;
+    }
+
+    sort(arr, arr + k);
+    i1.close();
+
+    i2.open("SORTED");
+    for (int i = 0; i < k; i++)
+    {
+        i2 << arr[i]<<endl;
+    }
+    i2.close();
+
+    cout << "Contents of the ORIGINAL file : ";
+    
+    ifile.open("ORIGINAL");
+    
+    for (int i = 0; i < k; i++)
+    {
+        int ele;
+        ifile >> ele;
+        cout << ele << " ";
+    }
+
+    ifile.close();
+
+    cout <<endl<< "Contents of the SORTED file : ";
+    
+    ifile.open("SORTED");
+    
+    for (int i = 0; i < k; i++)
+    {
+        int ele;
+        ifile >> ele;
+        cout << ele << " ";
+    }
+
+    ifile.close();
+    return 0;
+}
---
+++ file_IO.cpp
@@ -0,0 +1,65 @@
+/*Write a C++ program to show how class objects can be written to and read from the disk files. The length of the object is obtained using the sizeof operator. This length represents the sum total of lengths of all data members of the object.*/
+
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+class Emp
+{
+    char name[20];
+    int age;
+    long long phone_number;
+    
+    ofstream f1;
+    ifstream i1;
+
+    public:
+        Emp()
+        {
+            i1.open("input1");
+        }
+        void details()
+        {
+            i1>>name;
+            i1>>age;
+            i1>>phone_number;
+            i1.close();
+
+        }
+        void display_details()
+        {
+            cout<<"Name : "<<name<<endl;
+            cout<<"Age : "<<age<<endl;
+            cout<<"phone_number : "<<phone_number<<endl;
+        }
+        void write_to_file()
+        {
+            f1.open("output1");
+            f1<<name<<endl;
+            f1<<age<<endl;
+            f1<<phone_number<<endl;
+            f1.close();
+        }
+        
+};
+int main()
+{
+    Emp e1;
+    e1.details();
+    cout<<"Contents of the input file"<<endl;
+    e1.display_details();
+
+    e1.write_to_file();
+
+    cout<<"Contents of the output file."<<endl;
+    e1.display_details();
+
+    return 0;
+}
+
+
+
+
+
+
